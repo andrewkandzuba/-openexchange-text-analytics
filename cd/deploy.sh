d66fff0e2ff729abe3771eba829fd6829b2c6f10
andrew_kandzuba2 <andrey.kandzuba@gmail.com>
2023-01-04 20:13:23 -0600

    Fix copy jar files

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 gpg --fast-import /root/.gpg/gpg.key
-cp /jar/cli-*.jar cli/target/
+ls /jar/
+cp /jar/*.jar webservice/target/
 mvn deploy -P sign,build-extras,docker -DskipTests=true --settings cd/mvnsettings.xml
